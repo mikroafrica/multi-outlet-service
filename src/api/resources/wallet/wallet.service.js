22c4aa7e9ce24eeec228e11cd556edb8448d22af
Olalekan Kolapo <kolapo.pacholekan@gmail.com>
2020-12-04 12:21:15 +0100

    convert try catch to promise call

--- src/api/resources/wallet/wallet.service.js
+++ src/api/resources/wallet/wallet.service.js
@@ -13,54 +13,56 @@ export const walletTransactionsById = async ({
   limit,
   transactionType,
 }) => {
-  try {
-    const responseData = await fetchWalletTransactions({
-      walletId,
-      page,
-      limit,
-      dateFrom,
-      dateTo,
-      transactionType,
+  return fetchWalletTransactions({
+    walletId,
+    page,
+    limit,
+    dateFrom,
+    dateTo,
+    transactionType,
+  })
+    .then((responseData) => {
+      const walletTransactions = responseData.data;
+      return Promise.resolve({
+        statusCode: OK,
+        data: walletTransactions,
+      });
+    })
+    .catch((e) => {
+      logger.error(
+        `Error occurred while fetching wallet by id ${walletId} with error ${JSON.stringify(
+          e
+        )}`
+      );
+      return Promise.reject({
+        statusCode: BAD_REQUEST,
+        message:
+          JSON.parse(e.message).message ||
+          "Something went wrong. Please try again",
+      });
     });
-    const walletTransactions = responseData.data;
-    return Promise.resolve({
-      statusCode: OK,
-      data: walletTransactions,
-    });
-  } catch (e) {
-    logger.error(
-      `Error occurred while fetching wallet by id ${walletId} with error ${JSON.stringify(
-        e
-      )}`
-    );
-    return Promise.reject({
-      statusCode: BAD_REQUEST,
-      message:
-        JSON.parse(e.message).message ||
-        "Something went wrong. Please try again",
-    });
-  }
 };
 
 export const walletSummaryById = async ({ walletId }) => {
-  try {
-    const responseData = await fetchWalletSummaryById(walletId);
-    const walletSummary = responseData.data;
-    return Promise.resolve({
-      statusCode: OK,
-      data: walletSummary,
+  return fetchWalletSummaryById(walletId)
+    .then((responseData) => {
+      const walletSummary = responseData.data;
+      return Promise.resolve({
+        statusCode: OK,
+        data: walletSummary,
+      });
+    })
+    .catch((e) => {
+      logger.error(
+        `Error occurred while fetching wallet summary by id ${walletId} with error ${JSON.stringify(
+          e
+        )}`
+      );
+      return Promise.reject({
+        statusCode: BAD_REQUEST,
+        message:
+          JSON.parse(e.message).message ||
+          "Something went wrong. Please try again",
+      });
     });
-  } catch (e) {
-    logger.error(
-      `Error occurred while fetching wallet summary by id ${walletId} with error ${JSON.stringify(
-        e
-      )}`
-    );
-    return Promise.reject({
-      statusCode: BAD_REQUEST,
-      message:
-        JSON.parse(e.message).message ||
-        "Something went wrong. Please try again",
-    });
-  }
 };
