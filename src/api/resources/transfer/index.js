0cf7caa8adf05e3b1f54ff4f434ce0b79fb509ee
Olalekan Kolapo <kolapo.pacholekan@gmail.com>
2020-12-18 16:47:27 +0100

    fixed stuff

--- src/api/resources/transfer/index.js
+++ src/api/resources/transfer/index.js
@@ -1,8 +1,7 @@
-import { transferToOutlet, transferAcrossWallets } from "./transfer.controller";
+import { transferAcrossWallets } from "./transfer.controller";
 
 const transfer = ({ server, subBase }) => {
   server.post(`${subBase}/:id/:destination`, transferAcrossWallets);
-  // server.post(`${subBase}/:id/admin-to-outlet`, transferToOutlet);
 };
 
 export default transfer;
